1bf4d9ab356521fe924c30064ae81f62644e0c1a
Ghaizar <abu.razia13@gmail.com>
2023-01-14 22:04:34 +0800

    setting

--- setting/sss_computation.py
+++ setting/sss_computation.py
@@ -7,10 +7,13 @@ monthly_basic_salary  =  float(input('Amount: '))
 
 def sss_contri(salary):
 	if salary <= 4249.99:
-		contribution = {'msc': 4000.00, 'rer':380.00,'ree':180.00, 'ecc': 10.00}
+		contribution = {'mscec': 4000.00,'mscwisp':0, 'rer':380.00,'ree':180.00, 'ecc': 10.00,'wisper':0.00, 'wispee':0.00}
 
 	elif salary <= 4749.99:
-		contribution = {'msc': 4500.00, 'rer':380.00,'ree':180.00, 'ecc': 10.00}
+		contribution = {'mscec': 4500.00,'mscwisp':0, 'rer':427.50,'ree':202.50, 'ecc': 10.00,'wisper':0.00, 'wispee':0.00}
+
+	elif salary <= 5249.99:
+		contribution = {'mscec': 5000.00,'mscwisp':0, 'rer':475.00,'ree':225.00, 'ecc': 10.00,'wisper':0.00, 'wispee':0.00}
 
 
 
@@ -24,8 +27,6 @@ def sss_contri(salary):
 
 
 
-
-		
 
 	return print(contribution)
 
